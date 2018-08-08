bfbc0c2d93e3af885daaa0f46d1cd78e01f4b20a
Katarzyna Nieszporek <katarzyna.n.projekty@gmail.com>
2018-08-08 12:51:21 +0200

    Change Home.js

--- client/modules/Home/Home.js
+++ client/modules/Home/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+// import { bindActionCreators } from 'redux';
 
 // Import Style
 import styles from './Home.css';
@@ -9,22 +9,26 @@ import styles from './Home.css';
 class Home extends Component {
   render() {
     return (
+      <div>
+        <h2>Hello world! Hello everyone!</h2>
+        <p>Welcome to my blog. Here you will find some posts.</p>
+      </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {};
-};
+// const mapStateToProps = (state) => {
+//   return {};
+// };
 
-const mapDispatchToProps = (dispatch) => {
-  return {};
-};
+// const mapDispatchToProps = (dispatch) => {
+//   return {};
+// };
 
 Home.propTypes = {
 };
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+  // mapStateToProps,
+  // mapDispatchToProps
 )(Home);
